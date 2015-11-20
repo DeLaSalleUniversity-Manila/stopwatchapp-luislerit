88a19327341f61cc6142178197d5cae8181ce69d
luis <luis_lerit@yahoo.com>
2015-11-20 11:48:43 +0800

    stopwatch

---
+++ app/src/main/java/ph/edu/dlsu/stopwatch/MainActivity.java
@@ -0,0 +1,81 @@
+package ph.edu.dlsu.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int seconds = 0;
+    private boolean running;
+
+    //Start the stopwatch running when the Start button is clicked.
+    public void onClickStart(View view) {
+        running = true;
+    }
+    //Stop the stopwatch running when the Stop button is clicked.
+    public void onClickStop(View view) {
+        running = false;
+    }
+    //Reset the stopwatch when the Reset button is clicked.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d",
+                        hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
